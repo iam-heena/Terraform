331e1b82b674c8a3c75a39143dbf87a34b8c7bc5
HEENA <shaikheenakousar753@gmail.com>
2025-10-15 22:43:53 +0530

    incorect

--- Day-2/main.tf
+++ Day-2/main.tf
@@ -1,48 +1,10 @@
-# Create vpc
-resource "aws_vpc" "test" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name =  "Test_vpc"
-  }
-}
-#Create a subnet
-
-resource "aws_subnet" "test" {
-  vpc_id = aws_vpc.test.id
-  cidr_block = "10.0.0.0/24"
-  tags = {
-    Name = "test_sub"
-  }
-}
-#Create InternetGateway 
-resource "aws_internet_gateway" "test_IG" {
-  vpc_id = aws_vpc.test.id
-  tags = {
-    Name = "test_ig"
-  }
-}
-#Create Route Table and Edit 
-resource "aws_route_table" "test_RT" {
-    vpc_id = aws_vpc.test.id
-    route  {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.test_IG.id
-    
-    }
-    tags = {
-      Name = "Cust_RT"
-    }
-  
-}
-# Route Table association
-resource "aws_route_table_association" "RT" {
-    route_table_id = aws_route_table.test_RT.id
-    subnet_id = aws_subnet.test.id
-  
-}
 resource "aws_instance" "test" {
   ami = var.ami
   instance_type = var.instance_type
   key_name = var.key_name
   subnet_id = aws_subnet.test.id
+  associate_public_ip_address = true
+  tags = {
+    Name="Mahi"
+  }
 }
--- terraform.tfvars
+++
@@ -1,3 +0,0 @@
-ami = "ami-00ca32bbc84273381"
-instance_type = "t2.micro"
-key_name = "heena-key"
